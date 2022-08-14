94f73580765585f98cbd8dfc47ceafbb76ba75a7
lavr174 <lavr174@icloud.com>
2022-08-14 13:49:32 +0300

    task21

---
+++ task21/Program.cs
@@ -0,0 +1,9 @@
+﻿double Distance (double x1, double y1,
+                double z1,double x2, 
+                double y2, double z2)
+{
+    double dist = Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1));
+    return dist;
+}
+double mydist = Distance(7, -5, 0, 1, -1, 9);
+Console.WriteLine(mydist);
